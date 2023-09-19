a03c41635b76c41e965f20ce4fd2bf8d8e9af4f2
Xumpocmb <x.xumpocmb@gmail.com>
2023-09-19 10:42:13 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "hello"
+
+
+@app.route('about')
+def index():
+    return "about"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
